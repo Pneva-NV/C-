69ab184e293afc49f3866058f2b3a2af71fc50f9
Natalia-Pneva <pneva_nv@mail.ru>
2022-11-20 22:47:25 +0300

    Перемещение гирек

---
+++ Exemple6_Girya/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 7;
+int b = 3;
+int c = 15;
+int d = 9;
+int e = 5;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max=");
+Console.WriteLine(max);
