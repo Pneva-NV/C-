f8e6ec07d0985874fe79fe5181fd5adce246cb34
Natalia-Pneva <pneva_nv@mail.ru>
2022-11-20 22:32:13 +0300

    Приветствие особой персоны

---
+++ Exemple5_Persona/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите своё имя: ");
+string username = Console.ReadLine();
+
+if(username == "Миня")
+{
+    Console.WriteLine("Опять этот Миня");
+}
+else
+{
+    Console.Write("Привет дорогой, "); 
+    Console.Write(username);
+    Console.Write("!");
+}
