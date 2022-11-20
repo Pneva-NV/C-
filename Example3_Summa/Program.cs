0c65b02656eef42595a8cf2aa36dedebf00f47eb
Natalia-Pneva <pneva_nv@mail.ru>
2022-11-20 22:06:59 +0300

    Добавила рандомные числа

--- Example3_Summa/Program.cs
+++ Example3_Summa/Program.cs
@@ -1,4 +1,5 @@
-﻿int number1 = 5;
-int number2 = 8;
+﻿int number1 = new Random().Next(0, 107);
+Console.WriteLine(number1);
+int number2 = new Random().Next(0, 237);
+Console.WriteLine(number2);
 Console.WriteLine(number1 + number2);
-
