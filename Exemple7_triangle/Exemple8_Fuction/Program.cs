416eaa6d5a679a3a03afb0c9c64ccd15f27db5a2
Natalia-Pneva <pneva_nv@mail.ru>
2022-11-25 13:19:54 +0300

    Использование фукнции

---
+++ Exemple7_triangle/Exemple8_Fuction/Program.cs
@@ -0,0 +1,25 @@
+﻿int Max(int max1, int max2, int max3)
+{
+    int result = max1;
+    if (max2 > result) result = max2;
+    if (max3 > result) result = max3;
+    return result;
+}
+
+int a1 = 12;
+int b1 = 87;
+int c1 = 300;
+int a2 = 25;
+int b2 = 7;
+int c2 = 13;
+int a3 = 92;
+int b3 = 8;
+int c3 = 31;
+
+int max = Max( 
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+
+Console.WriteLine(max);
