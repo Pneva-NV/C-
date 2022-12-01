2123aaac82602014d8c7d21820246b2c1ec7eb42
Natalia-Pneva <pneva_nv@mail.ru>
2022-12-01 18:26:56 +0300

    Цикл FOR

---
+++ Exemple13_Cycle for/Program.cs
@@ -0,0 +1,12 @@
+﻿string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string result = Method4(2, "УРА!!!");
+Console.WriteLine(result);
